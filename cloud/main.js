9fa5ccbdadef43cfe7966afe67c3250864f14963
hicamelia <code@hicamelia.com>
2015-01-15 17:56:42 +0800

    main

--- cloud/main.js
+++ cloud/main.js
@@ -1,5 +1,5 @@
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
 AV.Cloud.define("hello", function(request, response) {
-  response.success("camelia山茶花服务器域名源自于十四君，樱花祭");
+  response.success("camelia山茶花服务器域名源自樱花祭");
 });
